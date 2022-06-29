3f986c08f21216704ce812c897ae74bb0fa87937
Simon Gilhooly <simon.gilhooly@atom14.co.uk>
2022-06-29 16:21:05 +0100

    Config for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+const ASSET_URL = process.env.ASSET_URL || '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  'base': `${ASSET_URL}/docs/`,
+  'build': {
+    'outDir': 'docs'
+  },
+  'plugins': [svelte()]
+});
